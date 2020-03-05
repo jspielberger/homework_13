d0cd5cbca797c1e60b15543c15ec2fcb38a479d7
Joel Spielberger <joelspielberger101@gmail.com>
2020-03-04 20:21:35 -0800

    hopefully fixed a bit of a port issue I had

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ var express = require("express");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
-
+var PORT = process.env.PORT || 3000;
 var exphbs = require("express-handlebars");
 
 // Sets up the Express app to handle data parsing
