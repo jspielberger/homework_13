d390f59e773375f2530f13618cb032921ba45302
Joel Spielberger <joelspielberger101@gmail.com>
2020-03-05 17:12:58 -0800

    messed with some heroku stuff, we'll see how it goes

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,16 @@
 var mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection.provess.env.JAWSDB_URL
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 } 
-else {
-connection = mysql.createConnection({
+else { connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "joelspielberger",
   database: "burgers_db"
 });
-}
+};
 
 connection.connect();
   
