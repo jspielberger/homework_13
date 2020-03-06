8bda77b507e4667a655afc9c8cedd04a318eca94
Joel Spielberger <joelspielberger101@gmail.com>
2020-03-05 16:49:32 -0800

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection.provess.env.JAWSDB_URL
+} 
+else {
+connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "joelspielberger",
   database: "burgers_db"
 });
+}
 
-connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+connection.connect();
   
-  module.exports = connection;
+module.exports = connection;
